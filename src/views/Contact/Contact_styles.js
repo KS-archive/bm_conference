2e9d6253d1efc52f68d00ebcedc04edceffa8c6d
KacperFloryn <floryn.kacper@gmail.com>
2018-01-14 18:03:50 +0100

    Add Contact View

---
+++ src/views/Contact/Contact_styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { colorPalette } from '../../utils/styles';
+
+export const Container = styled.div`
+    text-align: center;
+    color: black;
+    width: 90%;
+    background-color: #FEFE33;
+`;
