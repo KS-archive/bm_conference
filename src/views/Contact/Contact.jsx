ec7bda6ce6581394a1c378cc5a6fa134b41fa406
Unknown <mca.caterina@gmail.com>
2018-02-24 01:00:47 +0100

    Contact - update

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Container } from '../../utils/styledComponents';
-import { Header } from './Contact_styles';
+import { Header, Bold, Info, Line } from './Contact_styles';
+import { colorPalette } from '../../utils/styles';
 
 export default () => (
   <Container>
-    <Header>Zaczynamy contact!</Header>
+    <Header>KONTAKT</Header>
+    <Bold>Katedra Badań Marketingowych</Bold>
+    <Line>Uniwersytet Ekonomiczny we Wrocławiu</Line>
+    <Line>ul. Komandorska 118/120, 53-345 Wrocław</Line>
+    <Bold>dr hab. Magdalena Sobocińska</Bold>
+    <Line>tel.: (71) 36 80 247,</Line>
+    <Line>tel.: 501 396 764</Line>
+    <Line>e-mail: badania.markt@ue.wroc.pl</Line>
   </Container>
 );
