54124eabec886828144f90564258dbd785ea1659
Konrad Szwarc <konrad.szwarc@outlook.com>
2018-03-12 13:14:58 +0100

    Add contact

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Navbar from '../../components/Navbar/Navbar';
 import { Container } from '../../utils/styledComponents';
-import { Header } from './Contact_styles';
+import { Header, Bold, Line } from './Contact_styles';
 
 export default () => (
-  <Container>
-    <Header>Zaczynamy contact!</Header>
-  </Container>
+  <Fragment>
+    <Navbar activeIndex={5} />
+    <Container>
+      <Header>KONTAKT</Header>
+      <Bold>Katedra Badań Marketingowych</Bold>
+      <Line>Uniwersytet Ekonomiczny we Wrocławiu</Line>
+      <Line>ul. Komandorska 118/120, 53-345 Wrocław</Line>
+      <Bold>dr hab. Magdalena Sobocińska</Bold>
+      <Line>tel.: (71) 36 80 247,</Line>
+      <Line>tel.: 501 396 764</Line>
+      <Line>e-mail: badania.markt@ue.wroc.pl</Line>
+    </Container>
+  </Fragment>
 );
--- src/views/Contact/Contact_styles.js
+++ src/views/Contact/Contact_styles.js
@@ -2,8 +2,26 @@ import styled from 'styled-components';
 import { colorPalette } from '../../utils/styles';
 
 export const Header = styled.div`
+  font-weight: 500;
+  font-size: 25px;
   text-align: center;
-  color: black;
-  width: 90%;
-  background-color: #fefe33;
+  line-height: 1.4;
+  margin-top: 30px;
+  color: ${colorPalette.darkGray};
+`;
+
+export const Bold = styled.div`
+  margin-top: 20px;
+  font-weight: 500;
+  font-size: 15px;
+  text-align: center;
+  line-height: 1.4;
+  color: ${colorPalette.gray};
+`;
+
+export const Line = styled.div`
+  font-size: 15px;
+  text-align: center;
+  line-height: 1.4;
+  color: ${colorPalette.lightGray};
 `;
