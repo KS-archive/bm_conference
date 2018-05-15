5a80cf19493873f7a9e2008499f19bd94d874119
Konrad Szwarc <konrad.szwarc@outlook.com>
2018-05-15 13:08:30 +0200

    Add conference program

--- src/views/Program/Program.jsx
+++ src/views/Program/Program.jsx
@@ -1,20 +1,25 @@
 import React, { Fragment } from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import { Container } from '../../utils/styledComponents';
-import { Section, FontAwesome, Info } from './Program_styles';
+import { Section, Info, Download, DownloadItem } from './Program_styles';
 
 export default () => (
   <Fragment>
     <Navbar activeIndex={3} />
     <Container>
       <Section>
-        <FontAwesome>
-          <i className="fa fa-calendar fa-2x" aria-hidden="true" />
-        </FontAwesome>
         <Info>
-          Program konferencji zostanie <br /> wkrótce opublikowany
+          Pełny program konferencji można pobrać klikając przycisk poniżej.
         </Info>
       </Section>
+      <Download>
+        <a href="https://drive.google.com/open?id=0B6OmG2FpHh5GYmdyTURyLUxfa3BXZUJiZnU3eWM4RElqWkE4">
+          <DownloadItem key="Item1">
+            <i className="fa fa-download" aria-hidden="true" />
+            Program konferencji
+          </DownloadItem>
+        </a>
+      </Download>
     </Container>
   </Fragment>
 );
--- src/views/Program/Program_styles.js
+++ src/views/Program/Program_styles.js
@@ -1,15 +1,11 @@
 import styled from 'styled-components';
+import { colorPalette } from '../../utils/styles';
 
 export const Section = styled.div`
   text-align: center;
   margin-top: 96px;
   font-family: "Roboto", sans-serif;
-  color: #ccc;
-`;
-
-export const FontAwesome = styled.div`
-  text-align: center;
-  font-size: 72px;
+  color: #333;
 `;
 
 export const Info = styled.div`
@@ -19,3 +15,34 @@ export const Info = styled.div`
   font-weight: 400;
   font-size: 24px;
 `;
+
+export const Download = styled.div`
+  font-weight: 500;
+`;
+
+export const DownloadItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${colorPalette.blue};
+  margin: 15px auto;
+  color: white;
+  padding: 10px;
+  max-width: 500px;
+  border-radius: 3px;
+  border: 1px solid ${colorPalette.blue};
+  text-decoration: none;
+  transition: all 0.3s;
+  font-size: 15px;
+  line-height: 1.4;
+
+  i {
+    margin-right: 4px;
+  }
+
+  &:hover {
+    color: ${colorPalette.blue};
+    background-color: #fff;
+    border-color: ${colorPalette.blue};
+  }
+`
